fb534107ff60ea4d4f7f54679d670d9caffd2dba
tyagishi <tyagishi@gmail.com>
2025-02-04 14:50:56 +0900

    F - add init for Duration from Dates

--- Sources/SDSFoundationExtension/Duration/Duration_Init.swift
+++ Sources/SDSFoundationExtension/Duration/Duration_Init.swift
@@ -10,21 +10,14 @@ import Foundation
 @available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
 extension Duration {
     @available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
-    @inlinable public static func minutes(_ minutes: Double) -> Duration {
-        Duration(secondsComponent: Int64(minutes * 60), attosecondsComponent: 0)
-    }
-    
+    public init(from fromDate: Date, to toDate: Date) { self.init(secondsComponent: Int64(toDate.timeIntervalSince(fromDate)), attosecondsComponent: 0) }
+
     @available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
-    @inlinable public static func hours(_ hours: Double) -> Duration {
-        Duration(secondsComponent: Int64(hours * 60 * 60), attosecondsComponent: 0)
-    }
-    
+    @inlinable public static func minutes(_ minutes: Double) -> Duration { Duration(secondsComponent: Int64(minutes * 60), attosecondsComponent: 0) }
     @available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
-    @inlinable public static func days(_ days: Double) -> Duration {
-        Duration(secondsComponent: Int64(days * 24 * 60 * 60), attosecondsComponent: 0)
-    }
+    @inlinable public static func hours(_ hours: Double) -> Duration { Duration(secondsComponent: Int64(hours * 60 * 60), attosecondsComponent: 0) }
     @available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
-    @inlinable public static func weeks(_ weeks: Double) -> Duration {
-        Duration(secondsComponent: Int64(weeks * 7 * 24 * 60 * 60), attosecondsComponent: 0)
-    }
+    @inlinable public static func days(_ days: Double) -> Duration { Duration(secondsComponent: Int64(days * 24 * 60 * 60), attosecondsComponent: 0) }
+    @available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
+    @inlinable public static func weeks(_ weeks: Double) -> Duration { Duration(secondsComponent: Int64(weeks * 7 * 24 * 60 * 60), attosecondsComponent: 0) }
 }
