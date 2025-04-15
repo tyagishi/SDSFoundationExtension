6053758d6e5ecee15aaeef6a0f001964d0550541
tyagishi <tyagishi@gmail.com>
2025-04-15 21:07:00 +0900

    F - revise support os

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "SDSFoundationExtension",
     platforms: [
-        .iOS(.v16),
-        .macOS(.v12)
+        .macOS(.v12),
+        .iOS(.v15),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
