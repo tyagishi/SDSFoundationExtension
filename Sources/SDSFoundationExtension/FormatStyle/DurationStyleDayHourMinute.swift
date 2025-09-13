508aedb9f00e719aeb6f9a383650f494966e2443
tyagishi <tyagishi@gmail.com>
2025-09-13 11:07:42 +0900

    F - add new FormatStyle/ Duration.FormatStyle

--- Sources/SDSFoundationExtension/FormatStyle/DurationStyleDayHourMinute.swift
+++ Sources/SDSFoundationExtension/FormatStyle/DurationStyleDayHourMinute.swift
@@ -8,6 +8,93 @@
 import Foundation
 
 @available(macOS 13.0, iOS 16.0, tvOS 16.0, watchOS 9.0, *)
+extension Duration {
+    public struct FormatStyle: Foundation.FormatStyle {
+        public typealias FormatInput = Duration
+        public typealias FormatOutput = String
+        static let minute: Int64 = 60
+        static let hour: Int64 = 60 * 60
+        static let day: Int64 = hour * 24
+        
+        public enum DurationUnit: String, RawRepresentable, Codable {
+            case days, hours, minutes, seconds
+        }
+        public enum DurationUnitStyle: String, RawRepresentable, Codable {
+            case noCare
+            case omitDaysIfPossible
+            case omitHoursAboveIfPossible
+            case omitMinutesAboveIfPossible
+            
+            var omitDays: Bool {
+                switch self {
+                case .omitDaysIfPossible, .omitHoursAboveIfPossible, .omitMinutesAboveIfPossible: return true
+                default: return false
+                }
+            }
+            var omitHours: Bool {
+                switch self {
+                case .omitHoursAboveIfPossible, .omitMinutesAboveIfPossible: return true
+                default: return false
+                }
+            }
+            var omitMinutes: Bool {
+                switch self {
+                case .omitMinutesAboveIfPossible: return true
+                default: return false
+                }
+            }
+        }
+        let unitStyle: DurationUnitStyle
+        
+        init(unitStyle: DurationUnitStyle = .noCare) {
+            self.unitStyle = unitStyle
+        }
+
+        public func format(_ value: FormatInput) -> FormatOutput {
+            let nums = divideNums(value.components.seconds)
+            
+            var baseString: String = ""
+            if !omit(nums, .hours, unitStyle) {
+                baseString += String(format: "%02d", Int(nums.hours))
+            }
+            if !omit(nums, .minutes, unitStyle) {
+                if !baseString.isEmpty { baseString += ":" }
+                baseString += String(format: "%02d", Int(nums.minutes))
+            }
+            if !omit(nums, .seconds, unitStyle) {
+                if !baseString.isEmpty { baseString += ":" }
+                baseString += String(format: "%02d", Int(nums.seconds))
+            }
+
+            if !omit(nums, .days, unitStyle) {
+                baseString = String(format: "%d", Int(nums.days)) + "days " + baseString
+            }
+            return baseString
+        }
+        
+        func divideNums(_ sec: Int64) -> (days: Int64, hours: Int64, minutes: Int64, seconds: Int64) {
+            let days = Int64(sec / Self.day)
+            let hours = Int64((sec - days * Self.day) / Self.hour)
+            let minutes = Int64((sec - days * Self.day - hours * Self.hour) / Self.minute)
+            let seconds = Int64((sec - days * Self.day - hours * Self.hour - minutes * Self.minute))
+            return (days, hours, minutes, seconds)
+        }
+        
+        public func omit(_ nums: (days: Int64, hours: Int64, minutes: Int64, seconds: Int64),_ unit: DurationUnit,_ style: DurationUnitStyle) -> Bool {
+            if style == .noCare { return false }
+            switch unit {
+            case .days:  return (style.omitDays == true) && (nums.days == 0)
+            case .hours: return (style.omitHours == true) && (omit(nums, .days, style) == true) && (nums.hours == 0)
+            case .minutes:
+                return (style.omitMinutes == true) && (omit(nums, .hours, style) == true) && (nums.minutes == 0)
+            case .seconds: return false
+            }
+        }
+    }
+}
+
+@available(macOS 13.0, iOS 16.0, tvOS 16.0, watchOS 9.0, *)
+@available(*, deprecated, renamed: "Duration.FormatStyle", message: "use Duration.FormatStyle instead")
 public struct DurationStyleDayHourMinute: FormatStyle {
     public typealias FormatInput = Duration
     public typealias FormatOutput = String
@@ -30,6 +117,12 @@ public struct DurationStyleDayHourMinute: FormatStyle {
 }
 
 @available(macOS 13.0, iOS 16.0, tvOS 16.0, watchOS 9.0, *)
+@available(*, deprecated, renamed: "Duration.FormatStyle", message: "use Duration.FormatStyle instead")
 extension FormatStyle where Self == DurationStyleDayHourMinute {
     public static var dayHourMinute: DurationStyleDayHourMinute { .init() }
+}
+
+@available(macOS 13.0, iOS 16.0, tvOS 16.0, watchOS 9.0, *)
+extension FormatStyle where Self == Duration.FormatStyle {
+    public static var dayHourMinute: Duration.FormatStyle { .init() }
 }
--- Tests/SDSFoundationExtensionTests/FormatStyle/DurationStyleDayHourMinute_Tests.swift
+++ Tests/SDSFoundationExtensionTests/FormatStyle/DurationStyleDayHourMinute_Tests.swift
@@ -9,12 +9,35 @@ import XCTest
 @testable import SDSFoundationExtension
 
 final class DurationStyleDayHourMinute_Tests: XCTestCase {
+    @available(*, deprecated, renamed: "Duration.FormatStyle", message: "use Duration.FormatStyle instead")
     func test_Existing() throws {
         let duration = Duration(secondsComponent: Int64(60.0*60.0*24.0*4.5), attosecondsComponent: 0)
 
         XCTAssertEqual(duration.formatted(.time(pattern: .hourMinuteSecond)), "108:00:00")
         XCTAssertEqual(duration.formatted(DurationStyleDayHourMinute()), "4days 12:00:00")
-        XCTAssertEqual(duration.formatted(.dayHourMinute), "4days 12:00:00")
+        XCTAssertEqual(duration.formatted(DurationStyleDayHourMinute.dayHourMinute), "4days 12:00:00")
+    }
+
+    func test_NewDurationFormatStyle() throws {
+        let duration = Duration(secondsComponent: Int64(60.0*60.0*24.0*4.5), attosecondsComponent: 0)
+
+        //XCTAssertEqual(duration.formatted(.time(pattern: .hourMinuteSecond)), "108:00:00")
+        XCTAssertEqual(duration.formatted(Duration.FormatStyle.init(unitStyle: .noCare)), "4days 12:00:00")
+//        XCTAssertEqual(duration.formatted(.dayHourMinute), "4days 12:00:00")
+    }
+
+    func test_NewDurationFormatStyle_OmitDays() throws {
+        let duration = Duration(secondsComponent: Int64(60.0*60.0*18.24), attosecondsComponent: 0)
+
+        XCTAssertEqual(duration.formatted(Duration.FormatStyle.init(unitStyle: .noCare)), "0days 18:14:24")
+        XCTAssertEqual(duration.formatted(Duration.FormatStyle.init(unitStyle: .omitDaysIfPossible)), "18:14:24")
+    }
+
+    func test_NewDurationFormatStyle_OmitDaysHours() throws {
+        let duration = Duration(secondsComponent: Int64(60.0*18.24), attosecondsComponent: 0)
+
+        XCTAssertEqual(duration.formatted(Duration.FormatStyle.init(unitStyle: .noCare)), "0days 00:18:14")
+        XCTAssertEqual(duration.formatted(Duration.FormatStyle.init(unitStyle: .omitHoursAboveIfPossible)), "18:14")
     }
 
 }
